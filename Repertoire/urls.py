d8522928f60a6b8bca12e2e199aa26f6ae223289
Nikola Manojlovic <nikola@nikolamanojlovic.com>
2016-09-13 23:52:49 -0400

    Added AddSong Page. Now gets stored directly into Dbase

--- Repertoire/urls.py
+++ Repertoire/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url 
 from . import views
 
+app_name = 'Repertoire'
 
 urlpatterns=[
 url(r'^$', views.Repertoire, name='index'),
+url(r'^AddSongPage', views.AddSongPage, name='AddSongPage'),
+url(r'^AddSong', views.AddSong, name='AddSong'),
 # url(r'^check$', views.check, name='check'),
 # url(r'^$', views.PageNotFound, name='PageNotFound'),
 ]
--- Repertoire/views.py
+++ Repertoire/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import song
 # Create your views here.
+
+
 
 def index(request):
 	return HttpResponse("Hello World")
 
-def check(request):
-	return HttpResponse("Check")
+# def check(request):
+# 	return HttpResponse("Check")
 
 def Repertoire(request):
-	return render(request, 'Repertoire/repertoire.html' )
+	SongsInRepertoire = song.objects.all().order_by('name')
+	return render(request, 'Repertoire/repertoire.html', { 'songs' : SongsInRepertoire} )
+
+def AddSongPage(request):
+	return render(request, 'Repertoire/AddSong.html' )
 
 def PageNotFound(request):
+	# raise Http404("Page Not Found")
 	return HttpResponse("Page Not Found")
+
+def AddSong(request):
+	s = song(name=request.POST['SongName'])
+	s.save()
+
+	a = AddSongPage(request)
+	return a
