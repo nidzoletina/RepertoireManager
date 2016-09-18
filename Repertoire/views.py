ce0f109963e8aad816a2947d86e95b791d80e982
Nikola Manojlovic <nikola@nikolamanojlovic.com>
2016-09-18 17:48:20 -0400

    Added Authentication protocol before page load. Must be signed in to view pages.

--- Repertoire/views.py
+++ Repertoire/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import song
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-
+@login_required
 def index(request):
 	SongsInRepertoire = song.objects.all().order_by('name')
 	return render(request, 'Repertoire/repertoire.html', { 'songs' : SongsInRepertoire} )
 
 # def check(request):
 # 	return HttpResponse("Check")
-
+# @login_required()
 def Repertoire(request):
+	# if not request.user_is_authenticated:
+	# 	return HttpResponse("Page Not Found")
+
 	SongsInRepertoire = song.objects.all().order_by('name')
 	return render(request, 'Repertoire/repertoire.html', { 'songs' : SongsInRepertoire} )
 
